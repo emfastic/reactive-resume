accfb93c5eeb5f47d1bcea5bfc2a45c635f34105
Jake Ottiger <ottigerjake@gmail.com>
2022-11-07 22:46:09 -0500

    bullets add and del properly

--- src/Components/ResumeItems/ExperienceDesc.jsx
+++ src/Components/ResumeItems/ExperienceDesc.jsx
@@ -1,16 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { updateKeyedObjectSection } from "../../server/index.js";
 
 function ExperienceDesc(props) {
 
     const [description, setDescription] = useState('');
+    const [delCounter, setDelCounter] = useState(0);
+
+    /* pass the description */
+    useEffect(() => {
+        props.passUpwards(description, element => setDescription(element), props.idx);
+        // theoretically only dependency should be description change, not sure tho (TEMP FIX)
+        // eslint-disable-next-line
+    }, [description]);
 
     function handleChange(input) {
         if (input.key === 'Enter') {
             props.addDesc()
         }
         if (input.key === 'Backspace' && input.target.value === '') {
-            props.delDesc()
+            setDelCounter(1);
+            if (delCounter === 1) {
+                props.delDesc()
+                setDelCounter(0);
+            }
         }
     }
 
--- src/Components/ResumeItems/Experiences.jsx
+++ src/Components/ResumeItems/Experiences.jsx
@@ -1,7 +1,6 @@
 import React, { useRef, useState } from "react";
 import { updateKeyedObjectSection } from "../../server/index.js";
 import ExperienceDesc from "./ExperienceDesc.jsx";
-import { experiences } from '../../server/cv-data';
 
 function Experiences() {
     let organization = useRef(null);
@@ -15,47 +14,111 @@ function Experiences() {
     const [show2, setShow2] = useState(false)
     const [show3, setShow3] = useState(false)
     const [show4, setShow4] = useState(false)
-    const [show5, setShow5] = useState(false)
-    const setArray = [true, () => setShow1(true), () => setShow2(true), () => setShow3(true), () => setShow4(true), () => setShow5(true)]
+    const [show5, setShow5] = useState(false);
+    const [valObjArray, setValObjArray] = useState([{description: null, setDescription: null}, {description: null, setDescription: null}, {description: null, setDescription: null}, {description: null, setDescription: null}, {description: null, setDescription: null}, {description: null, setDescription: null}]);
+    const setArray = [true, () => setShow1(!show1), () => setShow2(!show2), () => setShow3(!show3), () => setShow4(!show4), () => setShow5(!show5)]
     const [bulletArray, setBulletArray] = useState([true, false, false, false, false, false])
 
-    function showBullet(setArray, bulletArray, idx) {
-        const trueIndices = bulletArray.map((val, idx) => val ? idx : undefined).filter(x => x !== undefined);
-        for (let i = 0; i < 6; ++i) {
-            if (trueIndices.find(element => element === i + 1) === undefined) {
-                setArray[i + 1]();
-                trueIndices[i + 1] = i + 1;
-                break;
-            }
+    /* show the next bullet on user return, shifting the vals to give appearance of next input being newly rendered */
+    function showBullet(call_idx, setArray, bulletArray) {
+        
+        // Find the first false element in shown state array
+        const firstFalse = bulletArray.findIndex(element => !element)
+        
+        // If all are true print message
+        // TODO: Render error message
+        if (firstFalse === -1) {
+            console.log('all are enabled')
+            return;
         }
-        const newBulletState = []
-        for (let j = 0; j < 6; ++j) {
-            console.log('gets here ')
-            if (trueIndices.find(element => element === j) !== undefined) {
-                newBulletState[j] = true
-            } else {
-                newBulletState[j] = false
-            }
+        
+        // Update bullet array to contain new shown value
+        const newBulletArray = [...bulletArray];
+        newBulletArray[firstFalse] = true;
+        setBulletArray(newBulletArray);
+
+        // If input after call idx isn't shown, show and return
+        if (firstFalse - call_idx === 1) {
+            setArray[firstFalse]();
+            return;
         }
-        console.log("new bullet:", newBulletState);
-        setBulletArray(newBulletState);
+
+        // Store description for next element in temp
+        // Set next element to empty and set the following to next's initial val
+        valObjArray[call_idx + 1].setDescription('');
+
+        // Start at the position past the empty input
+        // Go to last position in list (5)
+        for (let i = call_idx + 1; i < 5; i++) {
+            // Set the next position to the val of the last (shifting the element's vals)
+            // Imitates a new input being rendered next to the previous
+            console.log(valObjArray[i].description)
+            valObjArray[i + 1].setDescription(valObjArray[i].description)
+        }
+
+        // Once the values are changed, show the new item (actually at the end)
+        setArray[firstFalse]();
     }
 
-    function removeBullet(setShow, idx) {
-        setShow(false)
-        const newBulletState = [...bulletArray];
-        newBulletState[idx] = false
-        setBulletArray(newBulletState)
+    /* pass the val state and set val state up to experiences; change val array on effect change */ 
+    function getRefsFromChild(description, setDescription, idx) {
+
+        // Dereference val obj state array (tracking exp desc vals)
+        const newValArray = [...valObjArray];
+
+        // Set the val array obj at position idx corresponding to component's idx
+        newValArray[idx].description = description;
+        newValArray[idx].setDescription = setDescription;
+
+        // Update val array
+        setValObjArray(newValArray);
+    }
+
+    function removeBullet(call_idx, setArray, bulletArray) {
+
+        // Find the last true element in shown state array
+        const lastTrue = bulletArray.lastIndexOf(true)
+
+        // Catch user trying to delete only bullet
+        // TODO: set error message that you need 1 bullet
+        if (lastTrue === 0) {
+            console.log('need at least 1 bullet');
+            return;
+        }
+        
+        // Update bullet array that has deleted value
+        const newBulletArray = [...bulletArray];
+        newBulletArray[lastTrue] = false;
+        setBulletArray(newBulletArray);
+
+        // If last bullet in the list: hide bullet and return
+        if (lastTrue === call_idx) {
+            setArray[lastTrue]();
+            return;
+        }
+
+        // Set last to be empty so when bullet is added it is empty again
+        valObjArray[lastTrue].setDescription('');
+
+        // Start at last true position and decrement to call idx
+        for (let i = lastTrue; i > call_idx; i--) {
+            // Set the next position to the val of the last (shifting the element's vals)
+            // Imitates a new input being rendered next to the previous
+            valObjArray[i - 1].setDescription(valObjArray[i].description)
+        }
+
+        // Once the values are changed, show the new item (actually at the end)
+        setArray[lastTrue]();
     }
 
     return (
         <>
-        <ExperienceDesc addDesc={() => {showBullet(setArray, bulletArray)}} key={0} shown={true}></ExperienceDesc>
-        <ExperienceDesc addDesc={() => {showBullet(setArray, bulletArray)}} delDesc={() => {removeBullet(setShow1, 1)}} key={1} shown={show1}></ExperienceDesc>
-        <ExperienceDesc addDesc={() => {showBullet(setArray, bulletArray)}} delDesc={() => {removeBullet(setShow2, 2)}} key={2} shown={show2}></ExperienceDesc>
-        <ExperienceDesc addDesc={() => {showBullet(setArray, bulletArray)}} delDesc={() => {removeBullet(setShow3, 3)}} key={3} shown={show3}></ExperienceDesc>
-        <ExperienceDesc addDesc={() => {showBullet(setArray, bulletArray)}} delDesc={() => {removeBullet(setShow4, 4)}} key={4} shown={show4}></ExperienceDesc>
-        <ExperienceDesc addDesc={() => {}} delDesc={() => {removeBullet(setShow5, 5)}} key={5} shown={show5}></ExperienceDesc>
+        <ExperienceDesc passUpwards={getRefsFromChild} delDesc={() => {removeBullet(0, setArray, bulletArray)}} addDesc={() => {showBullet(0, setArray, bulletArray)}} idx={0} shown={true}></ExperienceDesc>
+        <ExperienceDesc passUpwards={getRefsFromChild} delDesc={() => {removeBullet(1, setArray, bulletArray)}} addDesc={() => {showBullet(1, setArray, bulletArray)}} idx={1} shown={show1}></ExperienceDesc>
+        <ExperienceDesc passUpwards={getRefsFromChild} delDesc={() => {removeBullet(2, setArray, bulletArray)}} addDesc={() => {showBullet(2, setArray, bulletArray)}} idx={2} shown={show2}></ExperienceDesc>
+        <ExperienceDesc passUpwards={getRefsFromChild} delDesc={() => {removeBullet(3, setArray, bulletArray)}} addDesc={() => {showBullet(3, setArray, bulletArray)}} idx={3} shown={show3}></ExperienceDesc>
+        <ExperienceDesc passUpwards={getRefsFromChild} delDesc={() => {removeBullet(4, setArray, bulletArray)}} addDesc={() => {showBullet(4, setArray, bulletArray)}} idx={4} shown={show4}></ExperienceDesc>
+        <ExperienceDesc passUpwards={getRefsFromChild} delDesc={() => {removeBullet(5, setArray, bulletArray)}} addDesc={() => {showBullet(5, setArray, bulletArray)}} idx={5} shown={show5}></ExperienceDesc>
         <br/>
         <br/>
         <br/>
