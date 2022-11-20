799ed744c35fb351c5fb16fe04d76f94978135da
Jake Ottiger <ottigerjake@gmail.com>
2022-11-19 20:55:26 -0500

    split login page handled login and signup interaction

--- src/Components/SignIn.jsx
+++ src/Components/SignIn.jsx
@@ -10,22 +10,33 @@ function SignIn() {
 
     return (
         <>
+        <div className="page-left">
         <div className="title">Reactive Resume</div>
         <div className="tagline">Generate a resume without the pain of formatting.</div>
+        <span className="test">
         <img src={pencil} className="pencil"/>
         <span className="main-point">TRACK</span>
         <span className="point-desc">experiences across multiple fields</span>
+        </span>
         <br/>
+        <span className="test">
         <img src={build} className="build"/>
         <span className="main-point">BUILD</span>
         <span className="point-desc">one section at a time</span>
+        </span>
         <br/>
+        <span className="test">
         <img src={diamond} className="diamond"/>
         <span className="main-point">CREATE</span>
         <span className="point-desc">professional resumes</span>
-        <br/>
-        <button onClick={handleSignIn}>Sign Up with Google</button>
-        <button onClick={handleSignIn}>Login with Google</button>
+        </span>
+        </div>
+        <div className="page-right">
+        <a onClick={handleSignIn} className="signin-button login">Login with Google</a>
+        <div className="signup">
+        <text className="text">No account? Sign up with </text><a onClick={handleSignIn} className="google">Google</a>
+        </div>
+        </div>
         </>
     )
 };
